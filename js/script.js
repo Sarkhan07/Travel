65b8330646f7bd11a74782b69a61ea7185da8f2b
Sarkhan <abdullayevsarxan92@gmail.com>
2022-01-08 23:35:37 +0400

    slides

--- js/script.js
+++ js/script.js
@@ -37,7 +37,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
     // Timer
 
-    let deadline = '2021-12-27';
+    let deadline = '2022-1-10';
 
     function getTimeRemaining(endtime) {
         let t = Date.parse(endtime) - Date.parse(new Date()),
@@ -87,7 +87,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
         
     }
-}
+ }
 
     setClock('timer', deadline);
 
@@ -113,25 +113,140 @@ window.addEventListener('DOMContentLoaded', function() {
         
     });
 
-    class Options {
-        constructor(height, width, bg, fontSize, textAlign) {
-            this.height = height;
-            this.width = width;
-            this.bg = bg;
-            this.fontSize = fontSize;
-            this.textAlign = textAlign;
-        }
+    // Form promise
 
-        сreateDiv() {
-            let elem = document.createElement('div');
-            document.body.appendChild(elem);
-            let param = `height:${this.height}px; width:${this.width}px; background-color:${this.bg}; font-size:${this.fontSize}px; text-align:${this.textAlign}`;
-            elem.style.cssText = param;
-        }
+
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся!',
+        failure: 'Что-то пошло не так...'
+    };
+
+    let form = document.querySelector('.main-form'),
+        formBottom = document.getElementById('form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+        statusMessage.classList.add('status');
+        
+    function sendForm(form) {
+
+    
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
+            form.appendChild(statusMessage);
+
+            function postData(data) {
+                return new Promise (function(resolve, reject) {
+                    let request = new XMLHttpRequest();
+                    request.open('POST', 'server.php');
+                    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            
+                    let formData = new FormData(form);
+            
+                    let obj = {};
+                    formData.forEach(function(value, key) {
+                        obj[key] = value;
+                    });
+    
+                    let json = JSON.stringify(obj);
+            
+                    request.addEventListener('readystatechange', function() {
+                       if (request.readyState < 4) {
+                            resolve();
+                   } else if(request.readyState === 4 && request.status == 200) {
+                            resolve();
+                       } else {
+                            reject();
+                        }
+                    });
+    
+                    request.send(json);
+    
+                   
+                });
+            } //endpostdata
+         
+
+            function clearInput() {
+                for (let i = 0; i < input.length; i++) {
+                    input[i].value = '';
+                }
+            }
+
+            postData(form)
+            .then(() => statusMessage.innerHTML = message.success)
+            .catch(() => statusMessage.innerHTML = message.failure)
+            .then(clearInput);
+            
+        }); 
+       
+
     }
 
-    const item = new Options(300, 350, "red", 14, "center");
+    sendForm(form);
 
-    item.сreateDiv();
+    sendForm(formBottom);
 
+
+    //slider
+
+    let slideIndex  = 1,
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
+
+        showSlides(slideIndex);
+
+    function showSlides(n) {
+        if(n > slides.length) {   // если n больше количество всех слайдов то есть мы перелистали все фотки, то аргумент функции равно одному то есть первому,
+            slideIndex = 1;
+        }
+
+        if(n < 1 ) {
+            slideIndex = slides.length; // а если слайд меньше одного то он slideIndex равно общему количество слайдов т.е последнуму.
+        }
+
+        slides.forEach((item) => item.style.display = 'none');
+    //    for (let i = 1; i < slides.length; i++) {
+    //        slides[i],style.display = 'none';
+    //    } old version of first
+        dots.forEach((item) => item.classList.remove('dot-active'));
+
+        slides[slideIndex - 1].style.display = 'block';
+        dots[slideIndex - 1].classList.add('dot-active');
+        
+        
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+        showSlides(slideIndex = n); // когда мы кликаем на четвертую точку на месте n пишется 4, и таким образом slideIndex принимает 4 ку и так выполняется 4 слайд
+    }
+
+    prev.addEventListener('click', function() {
+        plusSlides(-1);
+    });
+
+    next.addEventListener('click', function() {
+        plusSlides(1);
+    });
+
+    dotsWrap.addEventListener('click', function(event) {
+        for (let i = 0; i < dots.length + 1; i++) { // здесь мы запускаем цикл на один раз больше
+            if(event.target.classList.contains('dot') && event.target == dots[i - 1]) {
+                currentSlide(i);
+            }
+        }  // если кликаем 4 точку, тогда индекс нашей точки будет 3( с нуля начинается) то сюда в квадратные скобки передается тройка dots[i - 1], и здесь не стыковка потому что мы кликаем на 4 кнопку, а он у нас тройка, и таким образом при кликая 4 точку в дот мы передаем 4 ую и таким образом в в куррентслиде тоже передается четвертая.
+        
+    });
+    
 });
+
+    
+    
